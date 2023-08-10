201001ba0d54fc2fa287f03ca3012c88eecf4c18
Ali Keshanian <ar.k@outlook.com>
2023-08-10 16:18:15 -0700

    Change the length of the description column to 500 character to fix the too long error on Render

--- backend/db/migrations/20230808221527-create-spot.js
+++ backend/db/migrations/20230808221527-create-spot.js
@@ -48,7 +48,7 @@ module.exports = {
           type: Sequelize.STRING,
         },
         description: {
-          type: Sequelize.STRING,
+          type: Sequelize.STRING(500),
         },
         price: {
           type: Sequelize.DECIMAL,
