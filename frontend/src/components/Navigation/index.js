e524d484ca0a1754ab4a44ba11b2d12e23cd11a3
Ali Keshanian <ar.k@outlook.com>
2023-09-25 11:29:50 -0700

    implement close dropdown menu when login or signup modals open and on logout

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,44 +1,41 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
 import "./Navigation.css";
-import logo from './images/logo.png';
+import logo from "./images/logo.png";
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
+  const sessionUser = useSelector(state => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-         <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    );
-  }
+  // let sessionLinks;
+  // if (sessionUser) {
+  //   sessionLinks = (
+  //     <li>
+  //       <ProfileButton user={sessionUser} />
+  //     </li>
+  //   );
+  // } else {
+  //   sessionLinks = (
+  //     <li>
+  //       <OpenModalButton
+  //         buttonText="Log In"
+  //         modalComponent={<LoginFormModal />}
+  //       />
+  //        <OpenModalButton
+  //         buttonText="Sign Up"
+  //         modalComponent={<SignupFormModal />}
+  //       />
+  //     </li>
+  //   );
+  // }
 
   return (
     <header>
       <div className="logo">
-      <Link exact to="/">
-        <img src={logo} alt="logo" />
-      </Link>
+        <Link exact to="/">
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
       <nav>
         <ul>
@@ -47,7 +44,9 @@ function Navigation({ isLoaded }) {
               Home
             </NavLink>
           </li>
-          {isLoaded && sessionLinks}
+          <li>
+            <ProfileButton user={sessionUser} />
+          </li>
         </ul>
       </nav>
     </header>
